893719ba47e81a640c3852dd4673e974d5a11a7f
hhfei <huaifei91@qq.com>
2020-06-17 19:28:31 +0800

    预添加数据库表缺失自动添加的类（为了解决将来升级程序，升级的代码包括数据库表升级）暂时没有用

---
+++ HWallpaper.Business/DBTableManage.cs
@@ -0,0 +1,28 @@
+﻿using HWallpaper.Common;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+
+namespace HWallpaper.Business
+{
+    // 暂时用不到了
+    public class DBTableManage
+    {
+        private static string createHistory = "";
+        private static string existSql = "SELECT count(*) FROM sqlite_master WHERE type='table' AND name = @TableName";
+        /// <summary>
+        /// 检查表History是否存在
+        /// </summary>
+        public void NotExistCreateHistory(bool autoCreate)
+        {
+            SQLiteParameter[] parameters = { new SQLiteParameter("@TableName", "History") };
+            object result = SQLiteHelper.ExecuteScalar(existSql, parameters);
+            if (result != DBNull.Value && Convert.ToInt32(result) == 0)
+            { 
+                // 不存在则创建表
+            }
+        }
+    }
+}
